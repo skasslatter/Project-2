e3ef3e15a46f9265ce08f350dc30d937e73da8cb
Yvana Wagter <yvana@wagter.nl>
2020-03-24 17:25:08 +0100

    download kinda working but not completely

--- routes/shoppinglist.js
+++ routes/shoppinglist.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const Recipe = require("../models/Recipe.js");
 
-app.get("/", (req,res)=> {
-  res.render("shoppinglist", {currentUser: req.session.currentUser});
+app.get("/:id", (req,res)=> {
+  Recipe
+  .findById(req.params.id)
+  .then(recipe => {
+    res.render("user/shoppinglist", {recipe: recipe, currentUser: req.session.currentUser});
+  })
+  .catch(err => {
+    console.log("this is an error", err);
+    res.send("error", err);
+  });
 });
 
 module.exports = app;
