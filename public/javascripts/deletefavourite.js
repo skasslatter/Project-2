5bb4bb1d1267601f2c5eb8ecfbe02bff21098f5d
Yvana Wagter <yvana@wagter.nl>
2020-03-26 16:07:06 +0100

    fixed bugs

---
+++ public/javascripts/deletefavourite.js
@@ -0,0 +1,19 @@
+const $recipeList = document.querySelector("#favorites-list");
+const $deleteButtonsRecipes = $recipeList.querySelectorAll("#recipe-delete");
+for (i=0; i < $deleteButtonsRecipes.length; i++){
+  $deleteButtonsRecipes[i].addEventListener("click", deleteRecipe);
+}
+
+function deleteRecipe(event) {
+  const $pushedButton = event.target;
+  const $recipeId = $pushedButton.closest('#favourite-recipe').querySelector("#recipeId")
+
+  axios
+    .post("/recipes/delete", {recipeId: $recipeId.innerHTML})
+    .then((response) => {
+      $recipeToDelete = $pushedButton.closest("#favourite-recipe")
+      $recipeList.removeChild($recipeToDelete);
+    })
+}
+
+
