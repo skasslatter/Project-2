7367c08210ed2f45acd832342722b9dad608b505
Yvana Wagter <yvana@wagter.nl>
2020-03-27 10:51:25 +0100

    styling login and signup

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const recipeSchema = new Schema({
     title: String,
     ingredients: [{
-      type : mongoose.Schema.ObjectId,
+      type : mongoose.Types.ObjectId,
       ref : 'ingredients'
     }],
     duration: {
